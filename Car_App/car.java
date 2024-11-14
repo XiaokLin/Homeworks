461d1763b4b1da3e1d3285d3660f82c5522eba1b
Xiao <158627128+XiaokLin@users.noreply.github.com>
2024-11-14 19:27:12 +0000

    Updated with task names and fields

---
+++ Car_App/car.java
@@ -0,0 +1,22 @@
+/*Welcome to the BARI Car Mechanic System
+
+Add a Car
+Edit a Car
+Set a Specific Car to “FIXED”
+Display All Cars in Inventory
+Delete a Car by ID
+Display All Cars with Service Costs Exceeding a Specified Price
+Set a Specific Car to “FIXED”
+Display Expected Profit from Fixing All Cars
+Exit
+
+*/
+
+public class car{
+    String car = "x";
+    int car_id = 0;
+    double service_costs = 0;
+    boolean car_setting = false;
+
+    
+    }
