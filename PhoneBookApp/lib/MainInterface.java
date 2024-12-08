61a05e26a2143dadae044c88f3af46e0cc63f972
Xiao <158627128+XiaokLin@users.noreply.github.com>
2024-12-08 08:56:07 +0000

    updated with better naming pratices

--- PhoneBookApp/lib/MainInterface.java
+++ PhoneBookApp/lib/MainInterface.java
@@ -2,25 +2,26 @@
 This contains the folder for the print functions used in the interface
 */
 package lib;
+
 public class MainInterface {
+    
+}
 
-}  
-
-    class User_Interface {
-        public void user_view(){
-            System.out.println("1. Add a phone entry.");
-            System.out.println("2. Edit a phone entry of a given first name and last name.");
-            System.out.println("3. Sort the PhoneBookDirectory.");
-            System.out.println("4. Search using Linear Search.");
-            System.out.println("5. Print user’s info");
-        }
+class UserInterface {
+    public void user_view() {
+        System.out.println("1. Add a phone entry.");
+        System.out.println("2. Edit a phone entry of a given first name and last name.");
+        System.out.println("3. Sort the PhoneBookDirectory.");
+        System.out.println("4. Search using Linear Search.");
+        System.out.println("5. Print user’s info");
     }
+}
 
-    class Admin_Interface extends User_Interface {
-        public void admin_view(){
-            System.out.println("6. Delete a phone entry of a given first name and last name.");
-            System.out.println("7. Search using Binary Search.");
-            System.out.println("8. Change Password.");
-            System.out.println("9. Change Username.");
-        }
+class AdminInterface extends UserInterface {
+    public void admin_view() {
+        System.out.println("6. Delete a phone entry of a given first name and last name.");
+        System.out.println("7. Search using Binary Search.");
+        System.out.println("8. Change Password.");
+        System.out.println("9. Change Username.");
     }
+}
